f0a941482bc8c93d1f29ae56cf9b4f4910dabe20
Tim Jacomb <timjacomb1@gmail.com>
2022-09-09 09:00:49 +0100

    Build page additions

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -38,21 +38,21 @@ function Settings() {
             </div>
             Security
           </a>
-          <h2 className="jenkins-sidebar__heading">System</h2>
-          <a className="jenkins-sidebar__item">
-            <div className="jenkins-sidebar__item__icon">
+          <h2 className={"jenkins-sidebar__heading"}>System</h2>
+          <a className={"jenkins-sidebar__item"}>
+            <div className={"jenkins-sidebar__item__icon"}>
               <AccessibilityOutline />
             </div>
             System information
           </a>
-          <a className="jenkins-sidebar__item">
-            <div className="jenkins-sidebar__item__icon">
+          <a className={"jenkins-sidebar__item"}>
+            <div className={"jenkins-sidebar__item__icon"}>
               <AlbumsOutline />
             </div>
             Load statistics
           </a>
           <a className="jenkins-sidebar__item">
-            <div className="jenkins-sidebar__item__icon">
+            <div className={"jenkins-sidebar__item__icon"}>
               <AlbumsOutline />
             </div>
             About Jenkins
@@ -63,16 +63,16 @@ function Settings() {
 
         <div className="jenkins-form-item">
           <label htmlFor="systemMessage" className="jenkins-form-label">System message</label>
-          <p className="jenkins-form-description">This message will be displayed at the top of the Jenkins main page. This can be useful for posting notifications to your users</p>
-          <textarea id="systemMessage" rows="5" className="jenkins-input"/>
+          <p className={"jenkins-form-description"}>This message will be displayed at the top of the Jenkins main page. This can be useful for posting notifications to your users</p>
+          <textarea id="systemMessage" rows="5" className={"jenkins-input"}/>
         </div>
 
-        <div className="jenkins-form-item">
-          <label htmlFor="numExecutors" className="jenkins-form-label">Number of executors</label>
-          <input type="number" defaultValue="0" id="numExecutors" className="jenkins-input"/>
+        <div className={"jenkins-form-item"}>
+          <label htmlFor="numExecutors" className={"jenkins-form-label"}>Number of executors</label>
+          <input type="number" defaultValue="0" id="numExecutors" className={"jenkins-input"}/>
         </div>
 
-        <button className="jenkins-button jenkins-button--primary" type="submit">Save</button>
+        <button className={"jenkins-button jenkins-button--primary"} type="submit">Save</button>
 
       </div>
     </div>
