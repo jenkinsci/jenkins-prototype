dc946d3e1b2ccf89431a3764ce48616fce11a141
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-09-18 21:52:58 +0100

    Improve UI on web app

--- src/pages/BuildMonitor.jsx
+++ src/pages/BuildMonitor.jsx
@@ -2,6 +2,17 @@ function BuildMonitor() {
   return (
     <div className="jenkins-body">
       <h1>Build monitor</h1>
+      <p style={{lineHeight: 1.66, fontWeight: 450}}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras laoreet hendrerit convallis. Vestibulum at sem quis sem dignissim condimentum vitae vel ante. Aliquam mollis porttitor massa, at tincidunt velit suscipit eget. Aliquam pulvinar quam metus, vel finibus ipsum vehicula quis. Cras eget pulvinar erat. Donec ut imperdiet tortor, sed pulvinar mi. Aenean elit nisl, bibendum a pulvinar non, porta vel leo. Maecenas ut velit purus. Duis non placerat est. Maecenas eu odio interdum, vehicula turpis vitae, ullamcorper ipsum. Cras mattis pretium tortor, a vestibulum enim tincidunt a. Nulla facilisi. Pellentesque odio turpis, egestas non interdum vitae, bibendum eget metus. Donec non placerat nisi.
+        <br/><br/>
+        Sed a elit ligula. Phasellus posuere, ex non rhoncus sollicitudin, arcu dolor pretium sapien, ac aliquet neque orci rutrum nulla. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi feugiat est et nulla vehicula, vitae lobortis mauris hendrerit. Etiam sit amet pretium nunc. Curabitur mollis urna arcu, non fermentum mauris blandit porttitor. Aliquam in faucibus dui, quis molestie mi. Duis vel risus luctus, commodo mauris at, scelerisque tellus. Ut eu augue blandit, semper justo quis, gravida erat. Praesent ac placerat justo, et tempor velit. Nunc nulla nunc, rhoncus vel odio non, dignissim congue purus. Nunc suscipit tincidunt arcu, id molestie libero gravida in. Vestibulum ut justo quis quam porta efficitur.
+        <br/><br/>
+        Nam porttitor nisi est, vitae hendrerit nibh tincidunt at. Donec imperdiet, magna nec vehicula rhoncus, ante nisi euismod massa, eu malesuada metus diam a lorem. Fusce quis urna nec turpis eleifend auctor et tempus nunc. Vivamus ultrices mauris libero, dignissim imperdiet ligula laoreet ut. Quisque erat enim, feugiat eget augue nec, viverra ornare risus. Nullam ac lacinia urna. Mauris aliquet ipsum vel massa volutpat vestibulum. Pellentesque placerat ultricies odio vitae cursus. Cras porttitor lorem id nunc lacinia rhoncus. Praesent elit eros, gravida vel nulla eget, posuere pharetra tortor. Quisque id ultricies ex. Ut auctor lorem in dui fermentum, ut vulputate leo iaculis. Duis eget porttitor magna, sed venenatis ligula.
+        <br/><br/>
+        Sed hendrerit nisl consequat, tristique est vitae, ullamcorper nisi. Fusce commodo euismod ultricies. Quisque pharetra libero ac metus tempus posuere. Nulla euismod dapibus ornare. Phasellus lobortis, justo sit amet aliquet varius, nisl enim volutpat elit, ac luctus metus lectus mollis eros. Quisque auctor est lacus, a commodo nisl pellentesque consectetur. Nam lacinia porttitor sem, non malesuada elit porttitor sed. Vestibulum vel interdum tortor, id varius nisl.
+        <br/><br/>
+        Praesent maximus lacinia massa, eu aliquet tellus mattis vitae. Fusce nibh eros, lobortis quis justo ac, ullamcorper eleifend augue. Duis molestie et urna in consequat. Phasellus pretium quam vitae hendrerit interdum. Nulla sed urna tempor, lacinia orci eget, porttitor ex. Sed a elit bibendum, hendrerit arcu quis, fermentum nibh. Vivamus luctus, nibh efficitur accumsan congue, lacus purus tincidunt lacus, at feugiat dolor magna sed enim.
+      </p>
     </div>
   );
 }
