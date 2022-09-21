a98664f08e48d955628d129b87ac917f7126f408
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-09-21 16:23:45 +0100

    Add theme toggle, fix tooltips on Chrome

--- src/components/panels/Favorites.jsx
+++ src/components/panels/Favorites.jsx
@@ -4,25 +4,25 @@ import {Link} from "react-router-dom";
 function Favorites() {
   return (
     <div className={"app-favorites"}>
-      <Link className={"app-favorites__item"}>
+      <Link to={"/project"} className={"app-favorites__item"}>
         <div className={"app-favorites__item__icon"}>
           <BookOutline/>
         </div>
         Scalemate/develop
       </Link>
-      <Link className={"app-favorites__item"}>
+      <Link to={"/project"} className={"app-favorites__item"}>
         <div className={"app-favorites__item__icon"}>
           <BookOutline/>
         </div>
         Paleta/develop
       </Link>
-      <Link className={"app-favorites__item"}>
+      <Link to={"/project"} className={"app-favorites__item"}>
         <div className={"app-favorites__item__icon"}>
           <SearchOutline/>
         </div>
         Pale
       </Link>
-      <Link className={"app-favorites__item"}>
+      <Link to={"/project"} className={"app-favorites__item"}>
         <div className={"app-favorites__item__icon"}>
           <BookOutline/>
         </div>
--- src/components/panels/ThemeSwitcher.jsx
+++ src/components/panels/ThemeSwitcher.jsx
@@ -1,21 +1,26 @@
 import {LaptopOutline, MoonOutline, SunnyOutline} from "react-ionicons";
 
 function ThemeSwitcher() {
+
+  function setTheme(theme) {
+    document.body.dataset["theme"] = theme
+  }
+
   return (
     <div className={"app-theme-switcher"}>
-      <button className={"app-theme-switcher__item"}>
+      <button className={"app-theme-switcher__item"} onClick={() => setTheme("light")}>
         <div className={"app-theme-switcher__item__icon"}>
           <SunnyOutline/>
         </div>
         Light
       </button>
-      <button className={"app-theme-switcher__item"}>
+      <button className={"app-theme-switcher__item"} onClick={() => setTheme("dark")}>
         <div className={"app-theme-switcher__item__icon"}>
           <MoonOutline/>
         </div>
         Dark
       </button>
-      <button className={"app-theme-switcher__item app-theme-switcher__item--selected"}>
+      <button className={"app-theme-switcher__item"} onClick={() => setTheme("auto")}>
         <div className={"app-theme-switcher__item__icon"}>
           <LaptopOutline/>
         </div>
