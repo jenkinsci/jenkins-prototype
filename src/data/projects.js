7eb7408fa52bd2907f1e5dbb457ef4311fa1af67
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-10-18 13:00:43 +0100

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,71 +13,71 @@ const projects = [
     weather: <SunIcon />,
     name: "Jenkins",
     lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastFailure: "2 days ago",
     lastDuration: "30s"
   },
   {
     state: <FailedIcon />,
     weather: <LightningIcon />,
     name: "acceptance-test-harness",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "14 hours ago",
+    lastFailure: "1 day ago",
     lastDuration: "54m"
   },
   {
     state: <UnstableIcon />,
     weather: <DrizzleIcon />,
     name: "core-pr-tester",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "7 hours ago",
+    lastFailure: "4 days ago",
     lastDuration: "3m"
   },
   {
     state: <PassingIcon />,
     weather: <SunIcon />,
     name: "jenkins-test-harness",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "2 hours ago",
+    lastFailure: "1 week ago",
     lastDuration: "20s"
   },
   {
     state: <PassingIcon />,
     weather: <DrizzleIcon />,
     name: "jenkins-test-harness-htmlunit",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "1 hour ago",
+    lastFailure: "1 day ago",
     lastDuration: "5m"
   },
   {
     state: <PassingIcon />,
     weather: <SunIcon />,
     name: "pom",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "22 hours ago",
+    lastFailure: "23 hours ago",
     lastDuration: "30s"
   },
   {
     state: <UnstableIcon />,
     weather: <CloudIcon />,
     name: "remoting",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "1 day ago",
+    lastFailure: "2 days ago",
     lastDuration: "44s"
   },
   {
     state: <FailedIcon />,
     weather: <LightningIcon />,
     name: "slave-installer-module",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "1 week ago",
+    lastFailure: "2 weeks ago",
     lastDuration: "1m"
   },
   {
     state: <PassingIcon />,
     weather: <SunCloudIcon />,
     name: "stapler",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "1 hour ago",
+    lastFailure: "1 week ago",
     lastDuration: "2m"
   },
   {
@@ -85,15 +85,15 @@ const projects = [
     weather: <SunIcon />,
     name: "windows-slave-installer-module",
     lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastFailure: "3 days ago",
     lastDuration: "4s"
   },
   {
     state: <FailedIcon />,
     weather: <SunCloudIcon />,
     name: "winstone",
-    lastSuccess: "16 hours ago",
-    lastFailure: "One day ago",
+    lastSuccess: "13 hours ago",
+    lastFailure: "2 days ago",
     lastDuration: "12s"
   }
 ]
